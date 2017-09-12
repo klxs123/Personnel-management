8218608560fb782e506660da4fd075122756350c
klxs123 <874337707@qq.com>
2017-09-12 14:18:26 +0800

    Add project files.

---
+++ Personnel management/Bank management.h
@@ -0,0 +1,42 @@
+#include <cstdio>
+#include <cstdbool>
+#include <cstring>
+#include <stdlib.h>
+#include <stdbool.h>
+#define ture 1
+#define false 0
+
+const int MAX_MANAGE = 20;
+static bool FLAG = 0;
+
+typedef struct _manage
+{
+	char Name[20];
+	double balance;
+}Manage, *ManagePtr;
+
+typedef struct _Node
+{
+	int acctNum;
+	char password[20];
+	Manage data;
+	struct _Node* next;
+}Node, *NodePtr;
+
+int updateData(NodePtr node,char[]);
+int enterChoice(void);                   //输出界面用来选择
+int deleteRecord(NodePtr *);
+int login(char[][20]);
+int login_accountdata(char manage[][20], char[]);
+int login_passworddata(char[], char[]);
+void Print_Data(Node *, char[]);
+void newRecord(NodePtr *, char[][20]);
+void Writedata(Node *, char manage[][20]);
+void Printf_data(NodePtr);
+void initialize(FILE *);
+void read_data(Node *);
+void Printf_updata(NodePtr);
+Node* readdata(NodePtr *, char[][20]);
+Node* return_data(Node *);
+Node* recordIndex(Node *, int);
+NodePtr changechoice(NodePtr);
